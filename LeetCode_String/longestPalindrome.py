748f36b8c7e8193c5ee651d1e67b5424550a0369
MiTeng0215 <35425497+MiTeng0215@users.noreply.github.com>
2019-06-23 18:54:38 +0800

    Update longestPalindrome.py

--- LeetCode_String/longestPalindrome.py
+++ LeetCode_String/longestPalindrome.py
@@ -6,6 +6,10 @@ def longestPalindrome(s):
     if(len(s) < 2):
         return s
     for i in range(len(s)):
+        ```
+        如果回文是奇数就是(s,i,i)
+        如果回文是偶数就是(s,i,i+1)
+        ```
         expandPalindrome(s,i,i);
         expandPalindrome(s,i,i+1);
     return s[start:maxLen]
@@ -13,6 +17,7 @@ def longestPalindrome(s):
 def expandPalindrome(s,j,k):
     global start
     global maxLen
+    #动态规划的思想找出回文数
     while(j>=0 and k< len(s) and s[j]==s[k]):
         j = j-1
         k = k+1
